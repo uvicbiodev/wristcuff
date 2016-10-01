9eec74a4a8dd5311d4b46a23542b87b76c666d5f
Andrew Woodward <andrewmw@uvic.ca>
2016-10-01 12:38:17 -0700

    added PulseSensorSimple dir

---
+++ ArduinoIDE/PulseSensorSimple/PulseSensorSimple.ino
@@ -0,0 +1,64 @@
+/*
+ *  Biodev
+ *  Pulse Sensor Monitor
+ *  October 2016
+ */
+
+
+#include <LiquidCrystal.h>
+
+
+// select the pins used on the LCD panel
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+
+
+// Volatile Variables, used in the interrupt service routine!
+volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
+volatile int Signal;                // holds the incoming raw data
+volatile int IBI = 600;             // int that holds the time interval between beats! Must be seeded! 
+volatile boolean Pulse = false;     // "True" when User's live heartbeat is detected. "False" when not a "live beat". 
+volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
+
+
+
+
+void setup(){
+  
+  Serial.begin(9600);             // we agree to talk fast!
+  interruptSetup();               // sets up to read Pulse Sensor signal every 2mS  
+
+  lcd.begin(16, 2);              // start the library
+  lcd.setCursor(0,0);
+  lcd.print("Analyzing");
+  
+}
+
+
+//  Where the Magic Happens
+void loop(){
+        
+    
+  if (QS == true){     // A Heartbeat Was Found
+                       // BPM and IBI have been Determined
+                       // Quantified Self "QS" true when arduino finds a heartbeat
+        //Try printing out to second line of LCD here
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Analyzing");
+        lcd.setCursor(0,1);
+        lcd.print("BPM:"); // print a simple message
+        lcd.setCursor(6,1);
+        lcd.print(BPM);
+       
+        QS = false;                      // reset the Quantified Self flag for next time    
+  }
+     
+  delay(20);                        
+}
+
+
+
+
+
+
