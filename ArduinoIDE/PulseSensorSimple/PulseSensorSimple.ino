18625c8a0dde23967cf2d3f31c57ca413c76dde1
Andrew Woodward <andrewmw@uvic.ca>
2016-10-15 14:09:06 -0700

    updated pulsesensorsimple.ino and currently partially working, need to improve BPM calculation and signal processing

--- ArduinoIDE/PulseSensorSimple/PulseSensorSimple.ino
+++ ArduinoIDE/PulseSensorSimple/PulseSensorSimple.ino
@@ -14,20 +14,22 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 int pulsePin = 1;
 
-// Volatile Variables, used in the interrupt service routine!
-volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
-volatile int Signal;                // holds the incoming raw data
-volatile int IBI = 600;             // int that holds the time interval between beats! Must be seeded! 
-volatile boolean Pulse = false;     // "True" when User's live heartbeat is detected. "False" when not a "live beat". 
-volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
-
-
+// Volatile Variables, used in the interrupt service routine
+volatile int BPM;                   // calculated heart rate, beats per minute
+volatile int Signal;                // incoming raw data
+volatile int IBI = 600;             // int that holds the time interval between beats 
+//volatile int powerPin = A2;
 
 
 void setup(){
+
+  // initialize the serial communications:
+  Serial.begin(9600);
+
+  //pinMode(powerPin, OUTPUT);
   
   interruptSetup();               // sets up to read Pulse Sensor signal every 2mS  
-
+  
   lcd.begin(16, 2);              // start the library
   lcd.setCursor(0,0);
   lcd.print("Analyzing");
@@ -37,24 +39,34 @@ void setup(){
 
 
 void loop(){
-        
-    
-  if (QS == true){     // A Heartbeat Was Found
-                       // BPM and IBI have been Determined
-                       // Quantified Self "QS" true when arduino finds a heartbeat
-        //Try printing out to second line of LCD here
-        lcd.clear();
-        lcd.setCursor(0,0);
-        lcd.print("Analyzing");
-        lcd.setCursor(0,1);
-        lcd.print("BPM:"); // print a simple message
-        lcd.setCursor(6,1);
-        lcd.print(BPM);
+ 
+  //Try printing out to second line of LCD here
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Analyzing");
+  lcd.setCursor(0,1);
+  lcd.print("BPM:"); // print a simple message
+  lcd.setCursor(6,1);
+  lcd.print(BPM);
+
+
+  /*
+  //Serial.print("Signal: ");
+  Serial.print(Signal);                         // serial print serial for testing
+  Serial.print("\n");
+
+   delay(2);
+  */
+  
+  Serial.print("BPM: ");
+  Serial.print(BPM);
+  Serial.print("\n");
+
+  
        
-        QS = false;                      // reset the Quantified Self flag for next time    
-  }
-     
-  delay(20);                        
+  delay(200); 
+
+  
 }
 
 
