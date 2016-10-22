6c8abc7049753d5e6682ece59954bec67e07a22c
Laila Abelseth <Laila@w134-87-189-139.wireless.uvic.ca>
2016-10-22 11:00:24 -0700

    accelerometer code tested and working - Oct 22 2016

--- ArduinoIDE/AccelerometerTest-oct_15_2016/AccelerometerTest-oct_15_2016.ino
+++ ArduinoIDE/AccelerometerTest-oct_15_2016/AccelerometerTest-oct_15_2016.ino
@@ -1,6 +1,6 @@
 
-const int groundpin = 18;             // analog input pin 4 -- ground
-const int powerpin = 19;              // analog input pin 5 -- voltage
+const int groundpin = A0;             // analog input pin 0 -- ground
+const int powerpin = 18;              // analog input pin 4 -- voltage
 const int xpin = A3;                  // x-axis of the accelerometer
 const int ypin = A2;                  // y-axis
 const int zpin = A1;                  // z-axis (only on 3-axis models)
@@ -8,9 +8,14 @@ const int zpin = A1;                  // z-axis (only on 3-axis models)
 const int arraySize = 10;
 int Xvalues[arraySize];
 int Yvalues[arraySize];
+int Zvalues[arraySize];
 int i = 0;
+int j = 0;
+int k = 0;
 int Xsum = 0;
-bool Flag = true;
+int Ysum = 0;
+int Zsum = 0;
+bool Flag = false;
 
 
 void setup(){
@@ -21,15 +26,51 @@ void loop(){
     Xvalues[i] = analogRead(xpin);
     Xsum = Xsum + Xvalues[i];
     i++;
-    Flag = false;
-  }
+    //Flag = false;
+  } 
  
-  if(i == arraySize){  
+  if(i == arraySize){   
     if(analogRead(xpin)/(float)(Xsum/arraySize) < 0.95 || analogRead(xpin)/(float)(Xsum/arraySize) > 1.05){
      i = 0;
      Xsum = 0;
-     Flag = true; 
+     Flag = false; 
+  }else{
+    Flag = true; 
+  }
+  }
+
+  if (j< arraySize){
+    Yvalues[j] = analogRead(ypin);
+    Ysum = Ysum + Yvalues[j];
+    j++;
+    //Flag = false;
+  }
+ 
+  if(j == arraySize){  
+    if(analogRead(ypin)/(float)(Ysum/arraySize) < 0.95 || analogRead(ypin)/(float)(Ysum/arraySize) > 1.05){
+     j = 0;
+     Ysum = 0;
+     Flag = false; 
+  }else{
+    Flag =true;
   } 
+  }
+  
+  if (k< arraySize){
+    Zvalues[k] = analogRead(zpin);
+    Zsum = Zsum + Zvalues[k];
+    k++;
+    //Flag = false;
+  }
+ 
+  if(k == arraySize){  
+    if(analogRead(zpin)/(float)(Zsum/arraySize) < 0.95 || analogRead(zpin)/(float)(Zsum/arraySize) > 1.05){
+     k = 0;
+     Zsum = 0;
+     Flag = false; 
+  }else{
+    Flag = true; 
+  }
   }
   
      // print the sensor values:
@@ -41,7 +82,13 @@ void loop(){
   Serial.print("\t");
   Serial.print(analogRead(zpin));
   Serial.print("\t");
-  Serial.print(Xsum/arraySize);
+  //Serial.print(Xsum/arraySize);
+  //Serial.print("\t");
+  //Serial.print(Ysum/arraySize);
+  //Serial.print("\t");
+  //Serial.print(Zsum/arraySize);
+  Serial.print(Flag);
+  
   
   Serial.println();
   // delay before next reading:
