f8f6b4ffcda837476513e7ad6c916a80d3d57704
Andrew Woodward <woodygoalie007@gmail.com>
2017-09-14 10:45:18 -0700

    added new testing for raw data from max30100

--- Stable/Wristcuff_Pulse_Bluetooth/Wristcuff_Pulse_Bluetooth.ino
+++ Stable/Wristcuff_Pulse_Bluetooth/Wristcuff_Pulse_Bluetooth.ino
@@ -38,7 +38,7 @@
 //TFT display libraries
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library
-#include <SPI.h>
+
 
 
 Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, rst);
